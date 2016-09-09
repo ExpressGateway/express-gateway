0900ed6856a4834c13274258eb119a4a8641f576
Roman Lisagor <rlisagor@gmail.com>
2016-09-09 12:58:40 -0700

    Basic JWT middleware working

--- src/processors/oauth2.js
+++ src/processors/oauth2.js
@@ -1,8 +1,35 @@
 'use strict';
 
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const debug = require('debug')('gateway:oauth2');
+
 function createOauth2Middleware(params) {
+  let jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeader(),
+    issuer: params.issuer,
+    audience: params.audience,
+    secretOrKey: params.key,
+    algorithms: params.algorithms
+  };
+
+  passport.use(new JwtStrategy(jwtOptions, (jwt, done) => {
+    done(null, jwt);
+  }));
   return function oauth2Middleware(req, res, next) {
-    next();
+    debug('authenticating with JWT token');
+    passport.authenticate('jwt', (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        debug(`authentication failed: ${info.message}`);
+        res.status(401).send(`Unauthorized (${info.message})`);
+        return;
+      }
+      return next();
+    })(req, res, next);
   };
 }
 
