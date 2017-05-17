8165420d98e6f077480f8012bef211baa37c3bda
DrMegavolt <drmegavolt@DrMegavoltMBP.local>
2017-05-17 16:37:10 +0300

    Custom Log levels per logger

--- src/log.js
+++ src/log.js
@@ -1,28 +1,28 @@
 const winston = require('winston');
 winston.loggers.add('EG:gateway', {
   console: {
-    level: process.env.LOG_LEVEL || 'error',
+    level: process.env.LOG_LEVEL_GATEWAY || process.env.LOG_LEVEL || 'error',
     colorize: true,
     label: 'EG:gateway'
   }
 });
 winston.loggers.add('EG:policy', {
   console: {
-    level: process.env.LOG_LEVEL || 'error',
+    level: process.env.LOG_LEVEL_POLICY || process.env.LOG_LEVEL || 'error',
     colorize: true,
     label: 'EG:policy'
   }
 });
 winston.loggers.add('EG:test', {
   console: {
-    level: process.env.LOG_LEVEL || 'error',
+    level: process.env.LOG_LEVEL_TEST || process.env.LOG_LEVEL || 'error',
     colorize: true,
     label: 'EG:test'
   }
 });
 winston.loggers.add('EG:config', {
   console: {
-    level: process.env.LOG_LEVEL || 'error',
+    level: process.env.LOG_LEVEL_CONFIG || process.env.LOG_LEVEL || 'error',
     colorize: true,
     label: 'EG:config'
   }
