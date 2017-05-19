fe9d022545b3638b5f1744340d13a183daaa3e70
James Wong <jameshsw@gmail.com>
2017-05-19 15:18:24 -0700

    Check key in all 3 places in req, changed debug to logger, and added tests

--- src/actions/keyauth.js
+++ src/actions/keyauth.js
@@ -1,20 +1,29 @@
 'use strict';
 
-const debug = require('debug')('gateway:keyauth');
+const logger = require('../log').gateway;
 // const consumers = require('../consumers');
 
 module.exports = function(req, res, next) {
-  debug('authenticating with API Key');
+  logger.debug(`key authentication`);
 	let apikey;
 	// let scopes;
 
-	apikey = req.query.apikey;
+	// check apikey in uuid format
+	if (req.query.apikey != null) {
+		apikey = req.query.apikey;
+	} else if (req.body.apikey != null) {
+		apikey = req.body.apikey;
+	} else {
+		apikey = req.headers['apikey'];
+	}
+
 	// scopes = req.context.scopes;
 
 	// placeholder: pending on credential to implement the authenticateKey logic
 	// if（!consumers.credentialService.authenticateKey(apikey, scopes)）{
 	if (apikey == null) {
 		// Unauthorized
+		logger.debug(`key authentication failed`);
 		res.status(401);
 		res.send('Unauthorized');
 	} else {
--- test/keyauth.test.js
+++ test/keyauth.test.js
@@ -6,21 +6,42 @@ let res;
 
 describe('key authentication tests', function(){
 
-	beforeEach(function(done) {
+	beforeEach(function() {
 		req = httpMocks.createRequest({
 			method: 'GET',
 			url: '/test/path'
 		});
 		res = httpMocks.createResponse();
-		done();
 	});
 
-	it('should authenticate', function(done) {
+	it('should authenticate key in query', function(done) {
 		req.query.apikey = '123';
 		keyauth(req, res, function next(error) {
 			if (error) { throw new Error('should not expect error'); }
 			if (res.status == 401) {
-				throw new Error('expected to authenticate'); 
+				throw new Error('expected to authenticate key in query'); 
+			}
+			done();
+		});
+	});
+
+	it('should authenticate key in body', function(done) {
+		req.body.apikey = '123';
+		keyauth(req, res, function next(error) {
+			if (error) { throw new Error('should not expect error'); }
+			if (res.status == 401) {
+				throw new Error('expected to authenticate key in body'); 
+			}
+			done();
+		});
+	});
+
+	it('should authenticate key in header', function(done) {
+		req.headers['apikey'] = '123';
+		keyauth(req, res, function next(error) {
+			if (error) { throw new Error('should not expect error'); }
+			if (res.status == 401) {
+				throw new Error('expected to authenticate key in header'); 
 			}
 			done();
 		});
