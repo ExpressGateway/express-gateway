04098cf877ed822218534dac6e8a74a00e77c698
Sergey Kuts <sergeykuc@gmail.com>
2017-05-24 17:36:24 +0300

    Feature/egcontext scopes connect #35  (#117)
    * egcontext provides apiEndpoint definition
    
    * test scopes are set to egContext

---
+++ test/routing/scopes-test.js
@@ -0,0 +1,37 @@
+let testHelper = require('./routing.helper')
+let appConfig = {
+  http: { port: 9089 },
+  apiEndpoints: {
+    test_default: {
+      scopes: [
+        { scope: 'admin', verbs: 'GET' },
+        { scope: 'profile', verbs: ['GET', 'POST'] }
+      ]
+    }
+  },
+  pipelines: {
+    pipeline1: {
+      apiEndpoints: ['test_default'],
+      policies: [{ action: { name: 'test_policy' } }]
+    }
+  }
+};
+
+describe('When scopes defined for apiEndpoint', () => {
+  let helper = testHelper()
+  before('setup', helper.setup({
+    fakeActions: ['test_policy'],
+    appConfig
+  }))
+  after('cleanup', helper.cleanup())
+  it('should set scopes to egContext', helper.validateSuccess({
+    setup: {
+      url: '/'
+    },
+    test: {
+      url: '/',
+      scopes: appConfig.apiEndpoints.test_default.scopes
+    }
+  }))
+
+})
