3d0d349f52dac6f4df9e2f52520883b96860da12
DrMegavolt <sergeykuc@gmail.com>
2017-05-25 10:18:22 +0300

    http + https started with the same config; tls tests to ensure SNI callback is working

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ let gateway = require('./gateway');
 
 if (require.main === module) {
   gateway.start({
-    configPath: process.argv[2] || '/etc/lunchbadger/gateway.conf',
-    defaultBindPort: 8080,
-    defaultBindHost: '127.0.0.1'
+    configPath: process.argv[2],
   });
 }
