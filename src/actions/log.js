bb04696e88b7652cbf315a08629048fe660e0b51
Sergey Kuts <sergeykuc@gmail.com>
2017-06-05 11:56:57 +0300

    implementation based on vm + doc update connect #64 (#124)
    * implementation based on vm + doc update
    
    * inspected output
    
    * removed util.inspect since result is a string; remove throw to avoid request failure

--- src/actions/log.js
+++ src/actions/log.js
@@ -1,15 +1,20 @@
 'use strict';
-
+const logger = require('../log').logPolicy;
 const ConfigurationError = require('../errors').ConfigurationError;
+const vm = require('vm');
 
 function createLogMiddleware(params) {
   if (!params || !params.message) {
     throw new ConfigurationError('Log middleware requires "message" param');
   }
+  let script = new vm.Script('`' + params.message + '`')
 
-  // eslint-disable-next-line no-unused-vars
   return function(req, res, next) {
-    //const message = eval('`' + params.message + '`');
+    try {
+      logger.info(script.runInNewContext(req))
+    } catch (e) {
+      logger.error("failed to build log message; " + e.message)
+    }
     next();
   };
 }
---
+++ test/policies/log/log.test.js
@@ -0,0 +1,41 @@
+const logAction = require('../../../src/actions/log').log
+const logger = require('../../../src/log').logPolicy;
+const sinon = require('sinon')
+const assert = require('assert')
+
+describe('logging policy', () => {
+  before('prepare mocks', () => {
+    sinon.spy(logger, 'info');
+    sinon.spy(logger, 'error');
+  })
+  it('should log url', () => {
+    let next = sinon.spy();
+    let logMiddleware = logAction({
+      message: '${url} ${method}'
+    })
+    logMiddleware({ url: '/test', method: 'GET' }, {}, next)
+    assert.equal(logger.info.getCall(0).args[0], '/test GET')
+    assert.ok(next.calledOnce)
+  })
+  it('should fail to access global context', () => {
+    let next = sinon.spy();
+    let logMiddleware = logAction({
+      message: '${process.exit(1)}'
+    })
+    logMiddleware({ url: '/test', method: 'GET' }, {}, next)
+    assert.ok(logger.info.notCalled)
+
+    assert.equal(logger.error.getCall(0).args[0], 'failed to build log message; process is not defined')
+    assert.ok(next.calledOnce)
+  })
+
+  afterEach(function() {
+    logger.info.reset();
+    logger.error.reset();
+  });
+
+  after(function() {
+    logger.info.restore();
+    logger.error.restore();
+  });
+})
