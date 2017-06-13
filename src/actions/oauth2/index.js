677c8530596b2f9573b43dad26d6a8946f64aae7
Irfan Baqui <irfanbaqui@gmail.com>
2017-06-13 11:17:23 -0500

    load express dependencies at the time of bootstrapping the app

--- src/actions/oauth2/index.js
+++ src/actions/oauth2/index.js
@@ -1,34 +1,19 @@
 'use strict';
 
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const errorHandler = require('errorhandler');
-const session = require('express-session');
-const passport = require('passport');
 const site = require('./site');
 const oauth2 = require('./oauth2');
 
-// Express configuration
-const app = express();
-app.set('view engine', 'ejs');
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(errorHandler());
-app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+module.exports = function(app) {
+  app.set('view engine', 'ejs');
+  require('./auth');
 
-// Passport configuration
-require('./auth');
+  app.get('/login', site.loginForm);
+  app.post('/login', site.login);
+  app.get('/logout', site.logout);
 
-app.get('/login', site.loginForm);
-app.post('/login', site.login);
-app.get('/logout', site.logout);
+  app.get('/oauth2/authorize', oauth2.authorization);
+  app.post('/oauth2/authorize/decision', oauth2.decision);
+  app.post('/oauth2/token', oauth2.token);
 
-app.get('/oauth2/authorize', oauth2.authorization);
-app.post('/oauth2/authorize/decision', oauth2.decision);
-app.post('/oauth2/token', oauth2.token);
-
-module.exports = app;
+  return app;
+}
