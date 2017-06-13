4c01d0e135fa2db0d95d9c840f47ec3041bffb51
Irfan Baqui <irfanbaqui@gmail.com>
2017-06-13 10:37:55 -0500

    Cleaned the oauth module of unnecessary code

--- src/actions/oauth2/index.js
+++ src/actions/oauth2/index.js
@@ -8,8 +8,6 @@ const session = require('express-session');
 const passport = require('passport');
 const site = require('./site');
 const oauth2 = require('./oauth2');
-const user = require('./user');
-const client = require('./client');
 
 // Express configuration
 const app = express();
@@ -25,19 +23,12 @@ app.use(passport.session());
 // Passport configuration
 require('./auth');
 
-app.get('/', site.index);
 app.get('/login', site.loginForm);
 app.post('/login', site.login);
 app.get('/logout', site.logout);
-app.get('/account', site.account);
 
 app.get('/oauth2/authorize', oauth2.authorization);
 app.post('/oauth2/authorize/decision', oauth2.decision);
 app.post('/oauth2/token', oauth2.token);
 
-app.get('/api/userinfo', user.info);
-app.get('/api/clientinfo', client.info);
-
 module.exports = app;
-
-//app.listen(process.env.PORT || 3000);
