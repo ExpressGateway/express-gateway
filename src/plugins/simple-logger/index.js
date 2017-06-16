52ce577057d94682f73710b7ec44c66730b4147c
DrMegavolt <sergeykuc@gmail.com>
2017-06-16 12:16:30 +0300

    plugin spec proposal

--- src/plugins/simple-logger/index.js
+++ src/plugins/simple-logger/index.js
@@ -1,5 +1,5 @@
 module.exports = (pluginManifest) => {
-  // pluginManifest {
+  // pluginManifest extends EventEmitter {
   //  consumerManagement {
   //    userSrv, appSrv, credentialSrv, authSrv, tokensSrv
   //  }
@@ -21,25 +21,26 @@ module.exports = (pluginManifest) => {
     // mainApp.use(my-final-error-handler(err,req,res,next)=>{})
   });
   pluginManifest.registerPolicy('simple-logger', (policyManifest) => {
-    // pluginManifest {
+    // pluginManifest extends EventEmitter{
     //  registerAction(actionName, actionParams=> express-middleware)
     //  registerCondition(conditionName, (req, conditionParams)=> {handler code})
     //  events:
     //    - pipeline:willMount  // will execute before EG loads current pipeline where policy is called
     //    - pipeline:didMount  // will execute after EG have loaded current pipeline where policy is called
     // }
-    policyManifest.registerCondition('pathExact', (req, actionConfig) => {
-      return req.url === actionConfig.path;
+    policyManifest.registerCondition('pathExact', (req, conditionParams) => {
+      // conditionParams - all properties specified in yml condition definition
+      return req.url === conditionParams.path;
     });
 
-    policyManifest.registerAction('log', (params) => {
-      if (!params || !params.message) {
+    policyManifest.registerAction('log', (actionParams) => {
+      if (!actionParams || !actionParams.message) {
         throw new pluginManifest.ConfigurationError('Log middleware requires "message" param');
       }
 
       return (req, res, next) => {
         try {
-          pluginManifest.logger.info(req.egContext.evaluateAsTemplateString(params.message));
+          pluginManifest.logger.info(req.egContext.evaluateAsTemplateString(actionParams.message));
         } catch (e) {
           pluginManifest.logger.error('failed to build log message; ' + e.message);
         }
