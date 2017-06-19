ecb636bffaa3e41f2d7fac6b41ed6735510e4a98
Kevin Swiber <kswiber@gmail.com>
2017-06-19 08:17:01 -0700

    Adding CLI support
    Adding CTRL-C handler to close database connection during input prompts for CLI.
    
    For CLI prompts, setting CTRL-C handler key buffer to UTF8 before comparison.
    
    Rebasing CLI off Consumer Management refactor, starting with eg user.
    
    Refactoring eg app CLI commands on top of new Consumer Management API.
    
    Fixing Linter errors.
    
    Rebasing CLI branch with new directory structure.
    
    Removing carets from CLI-related package.json dependencies for now.  Removing unhelpful comment.
    
    Getting Yeoman to work with Yargs.
    
    Moving all users CLI commands over to Yeoman.
    
    Making generators more Yeoman idiomatic.
    
    Adding Yeoman support for eg apps.
    
    Removing dead files, running linter.

--- bin/eg.js
+++ bin/eg.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 
-require('../src/cli/');
+require('../lib/cli/');
--- src/cli/commands/apps.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  command: ['apps <command> [options]', 'app'],
-  desc: 'Manage apps',
-  builder: yargs => yargs
-    .usage('Usage: $0 ' + process.argv[2] + ' <command> [options]')
-    .commandDir('apps')
-    .demandCommand(),
-  handler: yargs => {}
-};
