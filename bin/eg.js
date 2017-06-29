640d4ae75e634ee32650a6e794a11dbb32bb8ae5
Kevin Swiber <kswiber@gmail.com>
2017-06-29 12:28:53 -0700

    Removing unused function from bin/eg.js

--- bin/eg.js
+++ bin/eg.js
@@ -1,12 +1,4 @@
 module.exports = {
-  wrapConfig (yargs) {
-    return yargs
-      .string('config-dir')
-      .describe('config-dir', 'Directory for gateway configuration')
-      .nargs('config-dir', 1)
-      .group(['config-dir'], 'Configure:')
-      .help('h');
-  },
   exit () {
     const db = require('../lib/db')();
     db.quit();
