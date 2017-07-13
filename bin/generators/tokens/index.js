4f07e118339971b1ab52204bf8f44cee270660b3
DrMegavolt <sergeykuc@gmail.com>
2017-07-13 09:50:46 +0300

    token cli tests
    tokens API + CLI

---
+++ bin/generators/tokens/index.js
@@ -0,0 +1,17 @@
+const eg = require('../../eg');
+
+module.exports = class extends eg.Generator {
+  constructor (args, opts) {
+    super(args, opts);
+
+    this.configureCommand({
+      command: ['tokens <command> [options]', 'token'],
+      desc: 'Manage tokens',
+      builder: yargs => yargs
+        .reset()
+        .usage('Usage: $0 ' + process.argv[2] + ' <command> [options]')
+        .command(this.createSubCommand('revoke'))
+        .demandCommand()
+    });
+  }
+};
---
+++ test/cli/tokens/revoke.test.js
@@ -0,0 +1,109 @@
+const assert = require('assert');
+const request = require('supertest');
+let app = require('../../oauth/bootstrap');
+const environment = require('../../fixtures/cli/environment');
+const adminHelper = require('../../common/admin-helper')();
+const namespace = 'express-gateway:tokens:revoke';
+const idGen = require('uuid-base62');
+const authService = require('../../../lib/services').auth;
+
+describe('eg tokens revoke', () => {
+  let program, env, user, accessToken;
+  before(() => {
+    ({ program, env } = environment.bootstrap());
+    return adminHelper.start();
+  });
+  after(() => adminHelper.stop());
+
+  beforeEach(() => {
+    env.prepareHijack();
+    return adminHelper.admin.users.create({
+      username: idGen.v4(),
+      firstname: 'f',
+      lastname: 'l'
+    })
+    .then(createdUser => {
+      user = createdUser;
+      return adminHelper.admin.credentials.create(user.username, 'oauth2', {secret: 'test'});
+    })
+    .then(createdCred => {
+      // cred = createdCred;
+      return request(app)
+        .post('/oauth2/token')
+        .set('Authorization', 'basic ' + (Buffer.from(user.username + ':test').toString('base64')))
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .type('form')
+        .send({
+          grant_type: 'password',
+          username: user.username,
+          password: 'test'
+        })
+        .expect(200);
+    })
+    .then(res => {
+      let token = res.body;
+      assert.ok(token);
+      assert.ok(token.access_token);
+      accessToken = token.access_token;
+      assert.equal(token.token_type, 'Bearer');
+      return authService.authenticateToken(accessToken);
+    })
+    .then(res => {
+      assert.equal(res.consumer.username, user.username);
+    });
+  });
+
+  afterEach(() => {
+    env.resetHijack();
+    return adminHelper.reset();
+  });
+
+  it('revokes token', done => {
+    env.hijack(namespace, generator => {
+      let output = null;
+
+      generator.once('run', () => {
+        generator.log.ok = message => {
+          output = message;
+        };
+        generator.log.error = message => {
+          done(new Error(message));
+        };
+      });
+
+      generator.once('end', () => {
+        assert.equal(output, 'Access token has been revoked: ' + accessToken);
+        return authService.authenticateToken(accessToken).then(r => {
+          assert.ok(!r); // authentication failed
+          done();
+        }).catch(done);
+      });
+    });
+
+    env.argv = program.parse(`tokens revoke ` + accessToken);
+  });
+  it('revokes token -q', done => {
+    env.hijack(namespace, generator => {
+      let output = null;
+
+      generator.once('run', () => {
+        generator.stdout = message => {
+          output = message;
+        };
+        generator.log.error = message => {
+          done(new Error(message));
+        };
+      });
+
+      generator.once('end', () => {
+        assert.equal(output, accessToken);
+        return authService.authenticateToken(accessToken).then(r => {
+          assert.ok(!r); // authentication failed
+          done();
+        }).catch(done);
+      });
+    });
+
+    env.argv = program.parse(`tokens revoke -q ` + accessToken);
+  });
+});
