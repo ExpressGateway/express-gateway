4bbf37dfe9c173e1f7a216d1a031642981b06605
Kevin Swiber <kswiber@gmail.com>
2017-07-17 13:34:00 -0700

    Bugfix/hot reload connects #280 (#286)
    * Now using chokadir for config file watchers. Fixes #280.
    
    Closing watchers on CLI exit.
    
    Fixing hot reload tests.
    
    Removing unnecessary event wait in hot reload test.
    
    Bumping up the timeout for the hot-reload test.
    
    Revamping hot reload test.
    
    Adding awaitWriteFinish: true to watchers.
    
    Reverting test config gateway.config.yml.
    
    Reverting test/config/gateway.config.yml
    
    * Allowing hot reload to use old config on invalid gateway reload.
    
    * Moving hot-reload test config to its own fixture directory.
    
    * Adding an EG_DISABLE_CONFIG_WATCH parameter.  Refactor config so gateway can explicitly start watching.

--- test/common/server-helper.js
+++ test/common/server-helper.js
@@ -6,6 +6,7 @@ function generateBackendServer (port) {
   let app = express();
 
   app.all('*', (req, res) => {
+    const port = req.connection.server.address().port;
     res.send('Hello from port ' + port);
   });
   return new Promise((resolve) => {
