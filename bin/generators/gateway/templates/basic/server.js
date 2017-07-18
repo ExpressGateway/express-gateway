5b3faca099158611c269166fb166edf16d32ec28
Kevin Swiber <kswiber@gmail.com>
2017-07-18 11:21:38 -0700

    Creating a minimal libary API for index.js (#297)
    * Creating a minimal libary API for index.js
    
    * Adding test for main module.
    
    * Renaming config to configPath in Main class.

--- bin/generators/gateway/templates/basic/server.js
+++ bin/generators/gateway/templates/basic/server.js
@@ -1,9 +1,6 @@
 const path = require('path');
+const gateway = require('express-gateway');
 
-process.env.EG_CONFIG_DIR = path.join(__dirname, 'config');
-
-const startGateway = require('express-gateway/lib/gateway');
-const startAdminAPI = require('express-gateway/lib/rest');
-
-startGateway();
-startAdminAPI();
+gateway()
+  .load(path.join(__dirname, 'config'))
+  .run();
--- test/hot-reload.test.js
+++ test/hot-reload.test.js
@@ -140,55 +140,57 @@ describe('hot-reload', () => {
         awaitWriteFinish: true
       };
 
-      chokidar
-        .watch(testGatewayConfigPath, watchOptions)
-        .once('change', (evt) => {
-          request
-            .get(`http://localhost:${originalGatewayPort}`)
-            .end((err, res) => {
-              assert(err);
-              assert(res.clientError);
-              assert.equal(res.statusCode, 404);
-              done();
-            });
-        });
-
-      // remove key-auth policy
-      testGatewayConfigData.pipelines[0].policies.shift();
-
-      fs.writeFile(testGatewayConfigPath, yaml.dump(testGatewayConfigData), (err) => {
-        if (err) {
-          throw err;
-        }
+      const watcher = chokidar.watch(testGatewayConfigPath, watchOptions);
+      watcher.once('change', (evt) => {
+        request
+          .get(`http://localhost:${originalGatewayPort}`)
+          .end((err, res) => {
+            assert(err);
+            assert(res.clientError);
+            assert.equal(res.statusCode, 404);
+            done();
+          });
       });
-    }).timeout(5000);
+
+      watcher.on('ready', () => {
+        // remove key-auth policy
+        testGatewayConfigData.pipelines[0].policies.shift();
+
+        fs.writeFile(testGatewayConfigPath, yaml.dump(testGatewayConfigData), (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      });
+    }).timeout(10000);
 
     it('uses previous config on reload of invalid gateway.config.yml', done => {
       const watchOptions = {
         awaitWriteFinish: true
       };
 
-      chokidar
-        .watch(testGatewayConfigPath, watchOptions)
-        .once('change', (evt) => {
-          request
-            .get(`http://localhost:${originalGatewayPort}`)
-            .end((err, res) => {
-              assert(err);
-              assert(res.clientError);
-              assert.equal(res.statusCode, 404);
-              done();
-            });
-        });
-
-      // make config invalid
-      delete testGatewayConfigData.pipelines;
-
-      fs.writeFile(testGatewayConfigPath, yaml.dump(testGatewayConfigData), (err) => {
-        if (err) {
-          throw err;
-        }
+      const watcher = chokidar.watch(testGatewayConfigPath, watchOptions);
+      watcher.once('change', (evt) => {
+        request
+          .get(`http://localhost:${originalGatewayPort}`)
+          .end((err, res) => {
+            assert(err);
+            assert(res.clientError);
+            assert.equal(res.statusCode, 404);
+            done();
+          });
       });
-    }).timeout(5000);
+
+      watcher.on('ready', () => {
+        // make config invalid
+        delete testGatewayConfigData.pipelines;
+
+        fs.writeFile(testGatewayConfigPath, yaml.dump(testGatewayConfigData), (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      });
+    }).timeout(10000);
   });
 });
