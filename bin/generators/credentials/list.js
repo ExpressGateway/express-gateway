e1f02d632a2371208a2128881f2a7bf628fa9502
DrMegavolt <drmegavolt@DrMegavoltMBP.local>
2017-07-19 19:02:14 +0300

    list credentials by consumerId

---
+++ bin/generators/credentials/list.js
@@ -0,0 +1,40 @@
+const eg = require('../../eg');
+
+module.exports = class extends eg.Generator {
+  constructor (args, opts) {
+    super(args, opts);
+
+    this.configureCommand({
+      command: 'list [options]',
+      description: 'List all credentials for Consumer (User or App)',
+      builder: yargs =>
+        yargs
+          .usage(`Usage: $0 ${process.argv[2]} list [options]`)
+          .example(`$0 ${process.argv[2]} list -c 7498d1a9-7f90-4438-a9b7-0ba4c6022353`)
+          .string(['c'])
+          .describe('c', 'Consumer ID: can be User ID or username or app ID')
+          .alias('c', 'consumerId').nargs('c', 1)
+          .group(['c'], 'Options:')
+    });
+  }
+
+  prompting () {
+    return this.admin.credentials.list(this.argv.consumerId)
+      .then(data => {
+        let credentials = data.credentials;
+        if (!credentials || !credentials.length) {
+          return this.stdout('You have no credentials');
+        }
+        credentials.forEach(app => {
+          if (this.argv.q) {
+            this.stdout(app.id);
+          } else {
+            this.stdout(JSON.stringify(app, null, 2));
+          }
+        });
+      })
+      .catch(err => {
+        this.log.error(err.message);
+      });
+  }
+};
