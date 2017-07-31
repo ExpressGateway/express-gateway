d54891a0d51acba8a55a977a0c4a16f994d6a833
DrMegavolt <sergeykuc@gmail.com>
2017-07-31 18:23:12 +0300

    validated headers were sent

--- test/cli/users/list.test.js
+++ test/cli/users/list.test.js
@@ -3,6 +3,8 @@ const adminHelper = require('../../common/admin-helper')();
 const idGen = require('uuid-base62');
 const environment = require('../../fixtures/cli/environment');
 const namespace = 'express-gateway:users:list';
+const superagent = require('superagent');
+const sinon = require('sinon');
 
 describe('eg users list', () => {
   let program, env, user1, user2;
@@ -82,5 +84,64 @@ describe('eg users list', () => {
     });
 
     env.argv = program.parse('users list --quiet ');
+  });
+
+  describe('when passing -H flag', () => {
+    before('set spy', () => {
+      sinon.spy(superagent.Request.prototype, 'set');
+    });
+    after('restore', () => {
+      superagent.Request.prototype.set.restore();
+    });
+    it('should send header', done => {
+      env.hijack(namespace, generator => {
+        let output = {};
+
+        generator.once('run', () => {
+          generator.log.error = message => {
+            done(new Error(message));
+          };
+          generator.stdout = message => {
+            output[message] = true;
+          };
+        });
+
+        generator.once('end', () => {
+          let args = superagent.Request.prototype.set.lastCall.args;
+          // .set('X','Y') (headerName,value)
+          assert.equal(args[0], 'X');
+          assert.equal(args[1], 'Y');
+          assert.ok(output[user1.username]);
+          assert.ok(output[user2.username]);
+          done();
+        });
+      });
+
+      env.argv = program.parse('users list -H "X:Y" -q');
+    });
+    it('should send multi headers', done => {
+      env.hijack(namespace, generator => {
+        let output = {};
+
+        generator.once('run', () => {
+          generator.log.error = message => {
+            done(new Error(message));
+          };
+          generator.stdout = message => {
+            output[message] = true;
+          };
+        });
+
+        generator.once('end', () => {
+          assert.ok(superagent.Request.prototype.set.calledWithMatch(sinon.match('X'), sinon.match('Y')));
+          assert.ok(superagent.Request.prototype.set.calledWithMatch(sinon.match('A'), sinon.match('B')));
+          assert.ok(output[user1.username]);
+          assert.ok(output[user2.username]);
+          done();
+        });
+      });
+
+      env.argv = program.parse('users list -H "X:Y" -H "A:B" -q');
+    });
   });
 });
