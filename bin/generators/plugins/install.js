1f2cac9527cdd99eeca5dbe8a8469116fd5bf829
Kevin Swiber <kswiber@gmail.com>
2017-09-12 21:48:51 -0700

    Adding plugin install e2e test.

--- bin/generators/plugins/install.js
+++ bin/generators/plugins/install.js
@@ -19,10 +19,64 @@ module.exports = class extends eg.Generator {
         yargs
           .usage(`Usage: $0 ${process.argv[2]} install <package> [options]`)
           .example(`$0 ${process.argv[2]} install express-gateway-plugin-url-rewrite`)
+          .option('n', {
+            alias: 'enable',
+            describe: 'Enable plugin in system config',
+            demandOption: false,
+            type: 'boolean'
+          })
+          .option('g', {
+            alias: 'update-gateway-config',
+            describe: 'Update policy whitelist in gateway config',
+            demandOption: false,
+            type: 'boolean'
+          })
+          .option('o', {
+            alias: 'option',
+            describe: 'User property in the form [-p \'foo=bar\']',
+            demandOption: false,
+            type: 'string'
+          })
     });
   }
 
   initializing () {
+    this.enablePlugin = this.argv.n || null;
+    this.addPoliciesToWhitelist = this.argv.g || null;
+
+    let optionValues = [];
+
+    const argv = this.argv;
+    if (argv.o) {
+      optionValues = Array.isArray(argv.o) ? argv.o : [argv.o];
+    }
+
+    let hasInvalidProperty = false;
+
+    if (optionValues.length) {
+      this.pluginOptions = {};
+
+      const self = this;
+      optionValues.forEach(o => {
+        const equalIndex = o.indexOf('=');
+
+        if (equalIndex === -1 || equalIndex === o.length - 1) {
+          this.log.error('invalid option:', o);
+          hasInvalidProperty = true;
+          return;
+        }
+
+        const key = o.substring(0, equalIndex);
+        const value = o.substring(equalIndex + 1);
+
+        self.pluginOptions[key] = value;
+      });
+    }
+
+    if (hasInvalidProperty) {
+      return;
+    }
+
     return this.installer.runNPMInstallation({
       packageSpecifier: this.argv.package,
       cwd: this.env.cwd,
@@ -36,59 +90,62 @@ module.exports = class extends eg.Generator {
 
     const previousPluginOptions = this.installer.existingPluginOptions;
 
-    const pluginQuestions = keys.map(key => {
+    const commandLineOptionKeys =
+      this.pluginOptions ? Object.keys(this.pluginOptions) : [];
+
+    const self = this;
+
+    commandLineOptionKeys.forEach(key => {
       const schema = optionsMeta[key];
-      return {
-        type: 'input',
-        name: `pluginOption${key}`,
-        message: `Set value for ${key}:`,
-        default: previousPluginOptions[key],
-        validate: input => {
-          const type = schema.type;
-
-          if (['string', 'boolean', 'number'].indexOf(type) === -1) {
-            this.log.error(
-              `Invalid plugin option: ${key}. Type must be string, boolean, ` +
-              'or number.');
-
-            return false;
-          }
-
-          if (schema.required && !input) {
-            return false;
-          }
-
-          if (type === 'number' && isNaN(input)) {
-            return false;
-          }
-
-          if (type === 'boolean' && !(input === 'true' || input === 'false')) {
-            return false;
-          }
-
-          return true;
-        }
-      };
+      if (!self._validateOption(key, self.pluginOptions[key], schema)) {
+        this.log.error(`Invalid plugin option value for ${key}, ` +
+          `expected: ${schema.type || 'string'}`);
+      }
     });
 
-    return this.prompt(pluginQuestions.concat([
-      {
+    const pluginQuestions = keys
+      .filter(key => {
+        return commandLineOptionKeys.indexOf(key) === -1;
+      })
+      .map(key => {
+        const schema = optionsMeta[key];
+        return {
+          type: 'input',
+          name: `pluginOption${key}`,
+          message: `Set value for ${key}:`,
+          default: previousPluginOptions[key],
+          validate: input => {
+            return self._validateOption(key, input, schema);
+          }
+        };
+      });
+
+    const questions = pluginQuestions.slice(0);
+
+    if (this.enablePlugin === null) {
+      questions.push({
         type: 'confirm',
         name: 'enablePlugin',
         message: 'Would you like to enable this plugin in system config?'
-      },
-      {
+      });
+    }
+
+    if (this.addPoliciesToWhitelist === null) {
+      questions.push({
         type: 'confirm',
         name: 'addPoliciesToWhitelist',
         message: 'Would you like to add new policies to gateway config?'
-      }
-    ]))
+      });
+    }
+
+    return this.prompt(questions)
     .then(answers => {
-      this.enablePlugin = answers.enablePlugin;
-      this.addPoliciesToWhitelist = answers.addPoliciesToWhitelist;
+      this.enablePlugin = this.enablePlugin || answers.enablePlugin;
+      this.addPoliciesToWhitelist =
+        this.addPoliciesToWhitelist || answers.addPoliciesToWhitelist;
 
       if (pluginQuestions.length) {
-        this.pluginOptions = {};
+        this.pluginOptions = this.pluginOptions || {};
 
         const keys = pluginQuestions.map(opt => opt.name);
         const self = this;
@@ -122,5 +179,31 @@ module.exports = class extends eg.Generator {
 
   end () {
     this.stdout('Plugin installed!');
+  }
+
+  _validateOption (key, input, schema) {
+    const type = schema.type;
+
+    if (['string', 'boolean', 'number'].indexOf(type) === -1) {
+      this.log.error(
+        `Invalid plugin option: ${key}. Type must be string, boolean, ` +
+        'or number.');
+
+      return false;
+    }
+
+    if (schema.required && !input) {
+      return false;
+    }
+
+    if (type === 'number' && isNaN(input)) {
+      return false;
+    }
+
+    if (type === 'boolean' && !(input === 'true' || input === 'false')) {
+      return false;
+    }
+
+    return true;
   }
 };
--- test/common/cli.helper.js
+++ test/common/cli.helper.js
@@ -1,38 +1,47 @@
-const path = require('path');
 const { exec } = require('child_process');
-const os = require('os');
 const fs = require('fs');
-const uuid = require('node-uuid');
+const os = require('os');
+const path = require('path');
+const util = require('util');
+const tmp = require('tmp');
+
+require('util.promisify/shim')();
 
 const modulePath = path.resolve(__dirname, '..', '..', 'bin', 'index.js');
 
 module.exports.bootstrapFolder = function (options) {
-  let tempDir = path.join(os.tmpdir(), uuid.v4());
-  fs.mkdirSync(tempDir);
-  let execOptions = {
-    env: Object.assign({}, process.env)
-  };
-  let cmd = modulePath + '  gateway create -t getting-started -n test -d ' + tempDir;
-  return new Promise((resolve, reject) => {
-    exec(cmd, execOptions, function (error, stdout, stderr) {
-      if (error !== null) {
-        reject(error);
-      }
-      resolve({
-        basePath: tempDir,
-        configDirectoryPath: path.join(tempDir, 'config'),
-        gatewayConfigPath: path.join(tempDir, 'config', 'gateway.config.yml'),
-        systemConfigPath: path.join(tempDir, 'config', 'system.config.yml')
+  return util.promisify(tmp.dir)()
+    .then(tempDir => {
+      let execOptions = {
+        env: Object.assign({}, process.env)
+      };
+
+      let cmd = modulePath + ' gateway create -t getting-started -n test -d ' + tempDir;
+
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          exec(cmd, execOptions, function (error, stdout, stderr) {
+            if (error !== null) {
+              reject(error);
+            }
+            resolve({
+              basePath: tempDir,
+              configDirectoryPath: path.join(tempDir, 'config'),
+              gatewayConfigPath: path.join(tempDir, 'config', 'gateway.config.yml'),
+              systemConfigPath: path.join(tempDir, 'config', 'system.config.yml')
+            });
+          });
+        }, 1000);
       });
     });
-  });
 };
 
-module.exports.runCLICommand = function ({adminPort, adminUrl, configDirectoryPath, cliArgs}) {
+module.exports.runCLICommand = function ({adminPort, adminUrl, configDirectoryPath, cliArgs, cliExecOptions}) {
   // TODO: it should not depend on configFolder, API only, now the last dependency is models
-  let cliExecOptions = Object.assign({}, {
+  cliExecOptions = Object.assign({
     env: process.env
-  });
+  }, cliExecOptions || {});
+
   cliExecOptions.env.EG_CONFIG_DIR = configDirectoryPath;
   cliExecOptions.env.EG_ADMIN_URL = adminUrl || `http://localhost:${adminPort}`;
   const command = [modulePath].concat(cliArgs).join(' ');
