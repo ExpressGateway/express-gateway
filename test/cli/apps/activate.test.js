c197ed7513ab0f1014574d78b0145167bb4a9b0b
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2017-09-25 15:34:41 +0200

    Fixes unique app names

--- test/cli/apps/activate.test.js
+++ test/cli/apps/activate.test.js
@@ -37,7 +37,7 @@ describe('eg apps activate', () => {
     .then(createdApp => {
       app2 = createdApp;
       return adminHelper.admin.apps.create(user.id, {
-        name: 'appy2',
+        name: 'appy3',
         redirectUri: 'http://localhost:3000/cb'
       });
     })
