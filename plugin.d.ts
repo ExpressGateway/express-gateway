e906ba2d1c9a4b9ddf17a96d390ff74f1398b82a
Vincenzo Chianese <vincenz.chianese@icloud.com>
2017-09-28 20:18:08 +0200

    Add typing file for plugin

---
+++ plugin.d.ts
@@ -0,0 +1,37 @@
+import express = require("express");
+
+declare global {
+
+  namespace Express {
+    export interface Request {
+      egContext: any
+    }
+  }
+
+  namespace ExpressGateway {
+    interface Policy {
+      name: string,
+      policy(actionParams): express.RequestHandler
+    }
+
+    interface Condition {
+      name: string,
+      handler(req: express.Request, conditionConfig): boolean
+    }
+
+    interface PluginContext {
+      registerPolicy(policy: Policy): void,
+      registerCondition(condition: Condition): void,
+      registerGatewayRoute(gatewayRoutesDeclaration: Function): void,
+      registerAdminRoute(adminRoutesDeclaration: Function): void,
+      registerCLIExtension(cliExtension): void
+    }
+
+    export interface Plugin {
+      version: string,
+      policies: Array<string>,
+      init(context: PluginContext): void
+    }
+  }
+}
+
