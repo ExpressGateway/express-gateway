6f9cfe93ccebe6d39dea9a120b6afc70a65ee862
Vincenzo Chianese <vincenz.chianese@icloud.com>
2017-09-30 13:11:38 +0200

    Add type definitions for routes

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 import express = require("express");
+import { EventEmitter } from "events";
 
 declare global {
   namespace Express {
@@ -21,15 +22,17 @@ declare global {
     interface PluginContext {
       registerPolicy(policy: Policy): void,
       registerCondition(condition: Condition): void,
-      registerGatewayRoute(gatewayRoutesDeclaration: Function): void,
-      registerAdminRoute(adminRoutesDeclaration: Function): void,
+      registerGatewayRoute(gatewayRoutesDeclaration: (gatewayExpressApp: express.Application) => void): void,
+      registerAdminRoute(adminRoutesDeclaration: (adminExpressApp: express.Application) => void): void,
       registerCLIExtension(cliExtension): void
+      eventBus: EventEmitter
     }
 
     export interface Plugin {
       version: string,
       policies: Array<string>,
       init(context: PluginContext): void
+      options: object
     }
   }
 }
