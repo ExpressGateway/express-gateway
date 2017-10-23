44a871996f835d519b5854366b73d9661bcd7750
Vincenzo Chianese <vincenz.chianese@icloud.com>
2017-10-23 15:29:59 +0200

    Implement the filtering part on the client

--- bin/generators/credentials/list.js
+++ bin/generators/credentials/list.js
@@ -1,5 +1,12 @@
 const eg = require('../../eg');
 
+const filtersTable = {
+  active: (consumer) => consumer.isActive,
+  archived: (consumer) => !filtersTable.active(consumer)
+};
+
+const filterTypes = Object.keys(filtersTable);
+
 module.exports = class extends eg.Generator {
   constructor (args, opts) {
     super(args, opts);
@@ -11,27 +18,38 @@ module.exports = class extends eg.Generator {
         yargs
           .usage(`Usage: $0 ${process.argv[2]} list [options]`)
           .example(`$0 ${process.argv[2]} list -c 7498d1a9-7f90-4438-a9b7-0ba4c6022353`)
-          .string(['c', 'f'])
+          .group(['c', 'f'], 'Options:')
+          .string('c').alias('c', 'consumerId').nargs('c', 1).required('c')
           .describe('c', 'Consumer ID: can be User ID or username or app ID')
+
+          .array('f')
           .describe('f', 'Comma separated list of credential state (active, archived), default: active')
-          .alias('c', 'consumerId').nargs('c', 1).required('c')
           .alias('f', 'filter')
-          .coerce('f', (val) => val.toLowerCase())
-          .group(['c'], 'Options:')
+          .coerce('f', (filters) =>
+            filters.reduce((array, filter) => {
+              if (filterTypes.indexOf(filter) === -1) {
+                throw new Error(`Unrecognised filter type: ${filter}`);
+              }
+              return filtersTable[filter];
+            }, [])
+          )
     });
   }
 
   prompting () {
     const { consumerId, filter } = this.argv;
 
-    return this.admin.credentials.list(consumerId, filter)
+    return this.admin.credentials.list(consumerId)
       .then(data => {
-        const { credentials } = data;
+        let { credentials } = data;
 
         if (!credentials || !credentials.length) {
           this.log.error(`Consumer ${consumerId} has no credentials`);
           return;
         }
+
+        filter.forEach(f => { credentials = credentials.filter(f); });
+
         credentials.forEach(credential => {
           if (this.argv.q) {
             this.stdout(credential.id);
