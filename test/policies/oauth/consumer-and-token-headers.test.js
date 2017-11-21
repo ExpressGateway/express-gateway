631c94728f5bc75f4167073c8bbfae6b52f6a75f
Serhii Kuts <sergeykuc@gmail.com>
2017-11-21 04:28:32 +0200

    Feature/set headers policy connect #502 (#546)
    * refactor and cleanup of proxy policy to allow new strategies
    
    * forward-headers policy
    
    * forward-headers policy
    
    * policy rename
    
    * policy rename
    
    * bugfix
    
    * fixes
    
    * schema fix
    
    * add warning if headers is not configured

--- test/policies/oauth/consumer-and-token-headers.test.js
+++ test/policies/oauth/consumer-and-token-headers.test.js
@@ -1,5 +1,5 @@
+const idGen = require('uuid-base62');
 const session = require('supertest-session');
-const should = require('should');
 const qs = require('querystring');
 const url = require('url');
 const express = require('express');
@@ -18,14 +18,14 @@ const db = require('../../../lib/db');
 const testHelper = require('../../common/routing.helper');
 const config = require('../../../lib/config');
 const originalGatewayConfig = config.gatewayConfig;
-
-describe.skip('Request Headers with consumer and token information as part of auth policies', () => {
+let request;
+describe('Request @headers @proxy downstream @auth @key-auth', () => {
   const helper = testHelper();
   const spy = sinon.spy();
   let originalAppConfig, originalCredentialConfig, originalUserConfig;
   let user, application, token, app, backendServer;
 
-  before('setup', (done) => {
+  before('setup', () => {
     config.gatewayConfig = {
       http: { port: 0 },
       serviceEndpoints: {
@@ -40,13 +40,33 @@ describe.skip('Request Headers with consumer and token information as part of au
           scopes: ['authorizedScope']
         }
       },
-      policies: ['oauth2', 'proxy'],
+      policies: ['oauth2', 'proxy', 'headers'],
       pipelines: {
         pipeline1: {
           apiEndpoints: ['authorizedEndpoint'],
           policies: [
             { oauth2: {} },
-            { proxy: [{ action: { serviceEndpoint: 'backend' } }] }
+            {
+              'headers': [{
+                action: {
+                  headerPrefix: 'eg-',
+                  forwardHeaders: {
+                    'id': 'consumer.id',
+                    'consumer-name': 'consumer.name',
+                    'consumer-type': 'consumer.type',
+                    'requestID': 'requestID',
+                    'scopes': 'consumer.token.scopes'
+                  }
+                }
+              }]
+            },
+            {
+              proxy: [{
+                action: {
+                  serviceEndpoint: 'backend'
+                }
+              }]
+            }
           ]
         }
       }
@@ -72,97 +92,84 @@ describe.skip('Request Headers with consumer and token information as part of au
       email: { isRequired: false, isMutable: true }
     };
 
-    db.flushdb()
+    return db.flushdb()
       .then(function () {
         const user1 = {
-          username: 'irfanbaqui',
-          firstname: 'irfan',
-          lastname: 'baqui',
-          email: 'irfan@eg.com'
+          username: idGen.v4(),
+          firstname: 'test',
+          lastname: 'test',
+          email: 'test@eg.com'
         };
 
-        userService.insert(user1)
-          .then(_user => {
-            should.exist(_user);
-            user = _user;
-
-            const app1 = {
-              name: 'irfan_app',
-              redirectUri: 'https://some.host.com/some/route'
-            };
-
-            applicationService.insert(app1, user.id)
-              .then(_app => {
-                should.exist(_app);
-                application = _app;
-
-                return credentialService.insertScopes(['authorizedScope'])
-                  .then(() => {
-                    Promise.all([credentialService.insertCredential(application.id, 'oauth2', { secret: 'app-secret', scopes: ['authorizedScope'] }),
-                      credentialService.insertCredential(user.id, 'basic-auth', { password: 'password', scopes: ['authorizedScope'] })])
-                      .then(res => {
-                        should.exist(res);
-
-                        helper.setup()
-                          .then(apps => {
-                            app = apps.app;
-                            const request = session(app);
-
-                            request
-                              .post('/login')
-                              .query({
-                                username: user.username,
-                                password: 'password'
-                              })
-                              .expect(302)
-                              .end(function (err, res) {
-                                should.not.exist(err);
-
-                                request
-                                  .get('/oauth2/authorize')
-                                  .query({
-                                    redirect_uri: application.redirectUri,
-                                    response_type: 'token',
-                                    client_id: application.id,
-                                    scope: 'authorizedScope'
-                                  })
-                                  .expect(200)
-                                  .end(function (err, res) {
-                                    should.not.exist(err);
-
-                                    request
-                                      .post('/oauth2/authorize/decision')
-                                      .query({
-                                        transaction_id: res.headers.transaction_id
-                                      })
-                                      .expect(302)
-                                      .end(function (err, res) {
-                                        should.not.exist(err);
-                                        const params = qs.parse(url.parse(res.headers.location).hash.slice(1));
-                                        token = params.access_token;
-
-                                        const backendApp = express();
-                                        backendApp.all('*', (req, res) => {
-                                          spy(req.headers);
-                                          res.send();
-                                        });
-
-                                        const runningBackendApp = backendApp.listen(7777, () => {
-                                          backendServer = runningBackendApp;
-                                          done();
-                                        });
-                                      });
-                                  });
-                              });
-                          });
-                      });
-                  });
-              });
-          });
+        return userService.insert(user1);
       })
-      .catch(function (err) {
-        should.not.exist(err);
-        done();
+      .then(_user => {
+        user = _user;
+        const app1 = {
+          name: idGen.v4(),
+          redirectUri: 'https://some.host.com/some/route'
+        };
+        return applicationService.insert(app1, user.id);
+      })
+      .then(_app => {
+        application = _app;
+        return credentialService.insertScopes(['authorizedScope']);
+      })
+      .then(() => {
+        return Promise.all([
+          credentialService.insertCredential(application.id, 'oauth2', { secret: 'app-secret', scopes: ['authorizedScope'] }),
+          credentialService.insertCredential(user.id, 'basic-auth', { password: 'password', scopes: ['authorizedScope'] })
+        ]);
+      })
+      .then(res => {
+        return helper.setup();
+      })
+      .then(apps => {
+        app = apps.app;
+        request = session(app);
+
+        return request
+          .post('/login')
+          .query({
+            username: user.username,
+            password: 'password'
+          })
+          .expect(302);
+      })
+      .then((res) => {
+        return request
+          .get('/oauth2/authorize')
+          .query({
+            redirect_uri: application.redirectUri,
+            response_type: 'token',
+            client_id: application.id,
+            scope: 'authorizedScope'
+          })
+          .expect(200);
+      })
+      .then((res) => {
+        return request
+          .post('/oauth2/authorize/decision')
+          .query({
+            transaction_id: res.headers.transaction_id
+          })
+          .expect(302);
+      })
+      .then((res) => {
+        const params = qs.parse(url.parse(res.headers.location).hash.slice(1));
+        token = params.access_token;
+
+        const backendApp = express();
+        backendApp.all('*', (req, res) => {
+          spy(req.headers);
+          res.send();
+        });
+        return new Promise(resolve => {
+          const runningBackendApp = backendApp.listen(7777, () => {
+            backendServer = runningBackendApp;
+            resolve();
+          });
+        });
       });
   });
 
@@ -176,40 +183,22 @@ describe.skip('Request Headers with consumer and token information as part of au
     done();
   });
 
-  it('should authenticate token for requests with scopes if requester is authorized', function (done) {
+  it('should authenticate token for requests with scopes if requester is authorized', function () {
     const request = session(app);
-    const expectedHeaders = [
-      'eg-token-scopes',
-      'eg-token-redirect-uri',
-      'eg-token-id',
-      'eg-token-expires-at',
-      'eg-token-created-at',
-      'eg-token-consumer-id',
-      'eg-token-authenticated-user-id',
-      'eg-token-auth-type',
-      'eg-consumer-user-id',
-      'eg-consumer-updated-at',
-      'eg-consumer-redirect-uri',
-      'eg-consumer-name',
-      'eg-consumer-is-active',
-      'eg-consumer-id',
-      'eg-consumer-created-at',
-      'eg-consumer-type'
-    ];
-
-    request
+    return request
       .get('/authorizedPath')
       .set('Authorization', 'bearer ' + token)
       .expect(200)
-      .end(function (err) {
-        should.not.exist(err);
+      .then(() => {
         assert(spy.calledOnce);
-        expectedHeaders.forEach(header => {
-          assert(Object.keys(spy.getCall(0).args[0]).indexOf(header) !== -1);
-        });
+        const headers = spy.getCall(0).args[0];
+        assert.equal(headers['eg-consumer-id'], application.id);
+        assert.equal(headers['eg-consumer-name'], application.name);
+        assert.equal(headers['eg-consumer-type'], 'application');
+        assert.ok(headers['eg-requestid']);
+        assert.equal(headers['eg-scopes'], 'authorizedScope');
 
         assert(Object.keys(spy.getCall(0).args[0]).indexOf('authorization') === -1);
-        done();
       });
   });
 });
