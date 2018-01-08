11b7572fe959b6804dfb070a226980d7d20df182
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-01-08 10:18:44 +0100

    Handle credential.json model

--- migrations/1515064228642-model-to-jsonschema.js
+++ migrations/1515064228642-model-to-jsonschema.js
@@ -1,34 +1,69 @@
+require('util.promisify/shim')();
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-require('util.promisify');
 
 const writeFile = util.promisify(fs.writeFile);
+const copyFile = util.promisify(fs.copyFile);
+const access = util.promisify(fs.access);
 
 module.exports.up = function () {
-  return Promise.all(['users', 'applications']
-    .map(model => ({ modelName: model, modelDefinition: require(path.join(process.env.EG_CONFIG_DIR, model)) }))
-    .map(({ modelName, modelDefinition }) => {
-      const newModel = {
-        type: 'object',
-        properties: {},
-        required: []
-      };
+  let modelPath;
+  if (process.env.EG_CONFIG_DIR) {
+    modelPath = path.join(process.env.EG_CONFIG_DIR, 'models');
+  } else {
+    modelPath = path.join(__dirname, '../../../', 'config/models');
+  }
 
-      if (modelDefinition.properties) {
-        Object.entries(modelDefinition.properties).forEach(([propertyName, propertyValue]) => {
-          newModel.properties[propertyName] = {
-            type: 'string'
-          };
+  return access(modelPath)
+    .then(() => Promise.all(
+      [copyFile(path.resolve('./lib/config/models/credentials.json'), path.join(modelPath, 'credentials.json'))]
+        .concat(['users', 'applications']
+          .map(model => ({ modelName: model, modelDefinition: require(path.join(modelPath, model)) }))
+          .map(({ modelName, modelDefinition }) => {
+            const newModel = {
+              $id: `http://express-gateway.io/models/${modelName}.json`,
+              type: 'object',
+              properties: {},
+              required: []
+            };
 
-          if (propertyValue.isRequired) {
-            newModel.required.push(propertyName);
-          }
-        });
-      }
+            if (modelDefinition.properties) {
+              Object.entries(modelDefinition.properties).forEach(([propertyName, propertyValue]) => {
+                newModel.properties[propertyName] = {
+                  type: 'string'
+                };
 
-      return { modelName, modelDefinition: newModel };
-    }).map(({ modelName, modelDefinition }) => writeFile(path.join(process.env.EG_CONFIG_DIR, modelName), modelDefinition)));
+                if (propertyName === 'email') {
+                  newModel.properties[propertyName].format = 'email';
+                }
+
+                if (propertyName === 'redirectUri') {
+                  newModel.properties[propertyName].format = 'uri';
+                }
+
+                if (propertyValue.isRequired) {
+                  newModel.required.push(propertyName);
+                }
+
+                if (modelName === 'users') {
+                  newModel.required.push('username');
+                  newModel.properties.username = {
+                    type: 'string'
+                  };
+                }
+              });
+            }
+
+            return { modelName, modelDefinition: newModel };
+          }).map(({ modelName, modelDefinition }) =>
+            writeFile(
+              path.join(process.env.EG_CONFIG_DIR, 'models', path.format({ name: modelName, ext: '.json' })),
+              JSON.stringify(modelDefinition, null, 2)
+            )
+          )
+        )
+    ));
 };
 
 module.exports.down = function (next) {
