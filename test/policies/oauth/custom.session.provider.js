8ab9b0aca45fd4ca4c23e05834fbef94c09453b1
DrMegavolt <sergeykuc@gmail.com>
2018-01-09 16:18:49 +0200

    test for custom session provider being inited

---
+++ test/policies/oauth/custom.session.provider.js
@@ -0,0 +1,58 @@
+const testHelper = require('../../common/routing.helper');
+const Config = require('../../../lib/config/config');
+const fakeSessionProvider = require('../../common/session-provider');
+const config = new Config();
+const assert = require('assert');
+
+describe('Functional Tests oAuth2.0 Policy', () => {
+  const helper = testHelper();
+
+  before('setup', () => {
+    config.systemConfig = {
+      db: {
+        redis: { emulate: true }},
+      session: {
+        storeProvider: '../../../test/common/session-provider',
+        storeOptions: {test: 45},
+        saveUninitialized: true,
+        resave: true,
+        secret: 'secret'
+      }
+    };
+    config.gatewayConfig = {
+      http: { port: 0 },
+      serviceEndpoints: {
+        backend: {
+          url: 'http://localhost:6069'
+        }
+      },
+      apiEndpoints: {
+        api: {
+          host: '*'
+        }
+      },
+      policies: ['oauth2', 'proxy'],
+      pipelines: {
+        pipeline1: {
+          apiEndpoints: ['api'],
+          policies: [
+            { oauth2: {} },
+            { proxy: [{ action: { serviceEndpoint: 'backend' } }] }
+          ]
+        }
+      }
+    };
+    return helper.setup({config});
+  });
+
+  after('cleanup', (done) => {
+    helper.cleanup();
+    fakeSessionProvider.reset();
+    done();
+  });
+
+  it('should init session provider', function () {
+    const opts = fakeSessionProvider.getOptions();
+    assert.equal(opts.test, 45);
+  });
+});
