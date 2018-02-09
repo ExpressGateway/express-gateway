f9ba64517ba1d095afa55d40dfc589e50b33826c
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-02-09 11:35:41 +0100

    Default to empty header object

--- admin/client.js
+++ admin/client.js
@@ -1,7 +1,7 @@
 const superagent = require('superagent');
 const superagentPrefix = require('superagent-prefix');
 
-module.exports = ({ baseUrl, headers }) =>
+module.exports = ({ baseUrl, headers = {} }) =>
   superagent
     .agent()
     .use(superagentPrefix(baseUrl))
