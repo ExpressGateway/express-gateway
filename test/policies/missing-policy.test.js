4353d6ee3906c214a1a52224a14e2cf6f2896a64
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-02-28 13:44:06 +0100

    Add tests for policy undeclared

---
+++ test/policies/missing-policy.test.js
@@ -0,0 +1,53 @@
+const should = require('should');
+
+const testHelper = require('../common/routing.helper');
+const config = require('../../lib/config');
+const originalGatewayConfig = config.gatewayConfig;
+
+describe('Missing policies', () => {
+  const helper = testHelper();
+  describe('Using a policy that is not installed', () => {
+    before('setup', () => {
+      config.gatewayConfig = {
+        http: { port: 0 },
+        policies: ['rewrite'],
+        pipelines: {
+          pipeline1: {
+            apiEndpoint: 'authorizedEndpoint',
+            policies: [{ rewrite: { action: { serviceEndpoint: 'backend' } } }]
+          }
+        }
+      };
+    });
+
+    it('should prevent the gateway from starting', () => should(() => helper.setup({ config })).throw('POLICY_NOT_FOUND'));
+  });
+
+  describe('Using a policy that is not listed in policies array', () => {
+    before('setup', () => {
+      config.gatewayConfig = {
+        http: { port: 0 },
+        policies: ['basic-auth'],
+        apiEndpoints: {
+          authorizedEndpoint: {
+            host: '*',
+            paths: ['/test']
+          }
+        },
+        pipelines: {
+          pipeline1: {
+            apiEndpoint: 'authorizedEndpoint',
+            policies: [{ proxy: { action: { serviceEndpoint: 'backend' } } }]
+          }
+        }
+      };
+    });
+
+    it('should prevent the gateway from starting', () => should(() => helper.setup({ config })).throw('POLICY_NOT_DECLARED'));
+  });
+
+  afterEach('cleanup', () => {
+    config.gatewayConfig = originalGatewayConfig;
+    return helper.cleanup();
+  });
+});
