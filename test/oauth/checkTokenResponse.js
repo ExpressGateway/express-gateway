38ee098152cd31fbc9c0d337dc320fafd7b3332a
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-04-04 16:01:54 +0200

    Add other checks

--- test/oauth/checkTokenResponse.js
+++ test/oauth/checkTokenResponse.js
@@ -3,4 +3,6 @@ const should = require('should');
 module.exports = (response, additionalProps = []) => {
   should(response).have.properties('access_token', 'expires_in', ...additionalProps);
   should(response.token_type).be.eql('Bearer');
+  should(response.access_token.length).be.greaterThan(15);
+  should(response.refresh_token.length).be.greaterThan(15);
 };
