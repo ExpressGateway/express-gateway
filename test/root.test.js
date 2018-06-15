54bfdb60314b60b9b8cb6d3d4148626d0533a868
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-06-15 13:52:35 +0200

    Remove the test logger from production code

--- test/root.test.js
+++
@@ -1,7 +0,0 @@
-const logger = require('../lib/logger').test;
-before(() => {
-  process.on('unhandledRejection', (reason, p) => {
-    logger.error('Unhandled Rejection at:', p, 'reason:', reason);
-  });
-})
-;
