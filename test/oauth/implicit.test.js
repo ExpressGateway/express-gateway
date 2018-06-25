960339d1f8b47732b30a613014f91b2ca9349a50
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-06-25 16:48:00 +0200

    Dedent oauth2 tests

--- test/oauth/implicit.test.js
+++ test/oauth/implicit.test.js
@@ -13,9 +13,10 @@ describe('Functional Test Implicit grant', function () {
 
   before(() => createOAuthScenario().then(([user, app]) => { fromDbUser = user; fromDbApp = app; }));
 
-  it('should grant access token when requesting without scopes', function (done) {
+  it('should grant access token when requesting without scopes', () => {
     const request = session(app);
-    request
+
+    return request
       .get('/oauth2/authorize')
       .query({
         redirect_uri: fromDbApp.redirectUri,
@@ -24,59 +25,53 @@ describe('Functional Test Implicit grant', function () {
       })
       .redirects(1)
       .expect(200)
-      .end(function (err, res) {
-        should.not.exist(err);
+      .then(res => {
         res.redirects.length.should.equal(1);
         res.redirects[0].should.containEql('/login');
-        request
+
+        return request
           .post('/login')
           .query({
             username: fromDbUser.username,
             password: 'user-secret'
           })
-          .expect(302)
-          .end(function (err, res) {
-            should.not.exist(err);
-            should.exist(res.headers.location);
-            res.headers.location.should.containEql('/oauth2/authorize');
-            request
-              .get('/oauth2/authorize')
-              .query({
-                redirect_uri: fromDbApp.redirectUri,
-                response_type: 'token',
-                client_id: fromDbApp.id
-              })
-              .expect(200)
-              .end(function (err, res) {
-                should.not.exist(err);
-                request
-                  .post('/oauth2/authorize/decision')
-                  .query({ transaction_id: res.headers.transaction_id })
-                  .expect(302)
-                  .end(function (err, res) {
-                    should.not.exist(err);
-                    should.exist(res.headers.location);
-                    res.headers.location.should.containEql(fromDbApp.redirectUri);
-                    const params = qs.parse(url.parse(res.headers.location).hash.slice(1));
-                    should.exist(params.access_token);
-                    should.exist(params.token_type);
+          .expect(302);
+      }).then(res => {
+        should.exist(res.headers.location);
+        res.headers.location.should.containEql('/oauth2/authorize');
 
-                    tokenService.get(params.access_token)
-                      .then(token => {
-                        should.exist(token);
-                        should.not.exist(token.scopes);
-                        [token.id, token.tokenDecrypted].should.eql(params.access_token.split('|'));
-                        done();
-                      });
-                  });
-              });
-          });
+        return request
+          .get('/oauth2/authorize')
+          .query({
+            redirect_uri: fromDbApp.redirectUri,
+            response_type: 'token',
+            client_id: fromDbApp.id
+          })
+          .expect(200);
+      }).then(res => {
+        return request
+          .post('/oauth2/authorize/decision')
+          .query({ transaction_id: res.headers.transaction_id })
+          .expect(302);
+      }).then(res => {
+        should.exist(res.headers.location);
+        res.headers.location.should.containEql(fromDbApp.redirectUri);
+        const params = qs.parse(url.parse(res.headers.location).hash.slice(1));
+        should.exist(params.access_token);
+        should.exist(params.token_type);
+
+        return Promise.all([tokenService.get(params.access_token), params]);
+      }).then(([token, params]) => {
+        should.exist(token);
+        should.not.exist(token.scopes);
+        [token.id, token.tokenDecrypted].should.eql(params.access_token.split('|'));
       });
   });
 
-  it('should grant access token with requesting with scopes and scopes are authorized', function (done) {
+  it('should grant access token with requesting with scopes and scopes are authorized', () => {
     const request = session(app);
-    request
+
+    return request
       .get('/oauth2/authorize')
       .query({
         redirect_uri: fromDbApp.redirectUri,
@@ -85,59 +80,52 @@ describe('Functional Test Implicit grant', function () {
       })
       .redirects(1)
       .expect(200)
-      .end(function (err, res) {
-        should.not.exist(err);
+      .then(res => {
         res.redirects.length.should.equal(1);
         res.redirects[0].should.containEql('/login');
-        request
+        return request
           .post('/login')
           .query({
             username: fromDbUser.username,
             password: 'user-secret'
           })
-          .expect(302)
-          .end(function (err, res) {
-            should.not.exist(err);
-            should.exist(res.headers.location);
-            res.headers.location.should.containEql('/oauth2/authorize');
-            request
-              .get('/oauth2/authorize')
-              .query({
-                redirect_uri: fromDbApp.redirectUri,
-                response_type: 'token',
-                client_id: fromDbApp.id,
-                scope: 'someScope'
-              })
-              .expect(200)
-              .end(function (err, res) {
-                should.not.exist(err);
-                request
-                  .post('/oauth2/authorize/decision')
-                  .query({ transaction_id: res.headers.transaction_id })
-                  .expect(302)
-                  .end(function (err, res) {
-                    should.not.exist(err);
-                    should.exist(res.headers.location);
-                    res.headers.location.should.containEql(fromDbApp.redirectUri);
-                    const params = qs.parse(url.parse(res.headers.location).hash.slice(1));
-                    should.exist(params.access_token);
-                    should.exist(params.token_type);
-                    tokenService.get(params.access_token)
-                      .then(token => {
-                        should.exist(token);
-                        token.scopes.should.eql(['someScope']);
-                        [token.id, token.tokenDecrypted].should.eql(params.access_token.split('|'));
-                        done();
-                      });
-                  });
-              });
-          });
+          .expect(302);
+      }).then(res => {
+        should.exist(res.headers.location);
+        res.headers.location.should.containEql('/oauth2/authorize');
+
+        return request
+          .get('/oauth2/authorize')
+          .query({
+            redirect_uri: fromDbApp.redirectUri,
+            response_type: 'token',
+            client_id: fromDbApp.id,
+            scope: 'someScope'
+          })
+          .expect(200);
+      }).then(res => {
+        return request
+          .post('/oauth2/authorize/decision')
+          .query({ transaction_id: res.headers.transaction_id })
+          .expect(302);
+      }).then(res => {
+        should.exist(res.headers.location);
+        res.headers.location.should.containEql(fromDbApp.redirectUri);
+        const params = qs.parse(url.parse(res.headers.location).hash.slice(1));
+        should.exist(params.access_token);
+        should.exist(params.token_type);
+        return Promise.all([tokenService.get(params.access_token), params]);
+      }).then(([token, params]) => {
+        should.exist(token);
+        token.scopes.should.eql(['someScope']);
+        [token.id, token.tokenDecrypted].should.eql(params.access_token.split('|'));
       });
   });
 
-  it('should not grant access token with requesting with scopes and scopes are unauthorized', function (done) {
+  it('should not grant access token with requesting with scopes and scopes are unauthorized', () => {
     const request = session(app);
-    request
+
+    return request
       .get('/oauth2/authorize')
       .query({
         redirect_uri: fromDbApp.redirectUri,
@@ -146,32 +134,34 @@ describe('Functional Test Implicit grant', function () {
       })
       .redirects(1)
       .expect(200)
-      .end(function (err, res) {
-        should.not.exist(err);
+      .then(res => {
         res.redirects.length.should.equal(1);
         res.redirects[0].should.containEql('/login');
-        request
+        return request
           .post('/login')
           .query({
             username: fromDbUser.username,
             password: 'user-secret'
           })
-          .expect(302)
-          .end(function (err, res) {
-            should.not.exist(err);
-            should.exist(res.headers.location);
-            res.headers.location.should.containEql('/oauth2/authorize');
+          .expect(302);
+      }).then(res => {
+        should.exist(res.headers.location);
+        res.headers.location.should.containEql('/oauth2/authorize');
+        return request
+          .get('/oauth2/authorize')
+          .query({
+            redirect_uri: fromDbApp.redirectUri,
+            response_type: 'token',
+            client_id: fromDbApp.id,
+            scope: 'someScope someUnauthorizedScope'
+          })
+          .expect(200)
+          .then(res =>
             request
-              .get('/oauth2/authorize')
-              .query({
-                redirect_uri: fromDbApp.redirectUri,
-                response_type: 'token',
-                client_id: fromDbApp.id,
-                scope: 'someScope, someUnauthorizedScope'
-              })
+              .post('/oauth2/authorize/decision')
+              .query({ transaction_id: res.headers.transaction_id })
               .expect(403)
-              .end(done);
-          });
+          );
       });
   });
 });
