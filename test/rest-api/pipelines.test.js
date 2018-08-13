132d3a198859b23f1b866450ee3fb43a56cc0c25
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-08-13 22:46:51 +0200

    Validate configuration on API Calls before disk saving (#782)
    * Validate and throw if provided config is invalid
    
    * Validate initial raw file
    
    * Modify tests that were previously broken
    
    * Throw a specific error when invalid config is detected
    
    * Add error handler for invalid config
    
    * Express is required anyway by EG
    
    * Remove the old error handler
    
    * Remove undocumented and unusable feature
    
    * Shims on top
    
    * A pipeline should have at least a policy
    
    * Fix pipeline rest related tests
    
    * Validate pipelines before saving the stuff on the disk
    
    * Add test that an invalid api call does not write anything to disk
    
    * Add another test case for wrong policy in pipeline
    
    * Fix broken test after validations
    
    * Do not recreate all the policies and the router, just validate the stuff
    
    * Fix general gateway json schema
    
    * Comments reorder

--- test/rest-api/pipelines.test.js
+++ test/rest-api/pipelines.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const should = require('should');
 const adminHelper = require('../common/admin-helper')();
 const Config = require('../../lib/config/config');
 const os = require('os');
@@ -21,16 +21,21 @@ describe('REST: pipelines', () => {
   describe('when no pipelines defined', () => {
     beforeEach(() => {
       const initialConfig = {
-        admin: { port: 0 }
+        admin: { port: 0 },
+        apiEndpoints: { api: { host: '*' } },
+        policies: ['proxy', 'terminate'],
+        serviceEndpoints: { backend: { url: 'http://localhost:1010' } }
       };
+
       fs.writeFileSync(config.gatewayConfigPath, yaml.dump(initialConfig));
       config.loadGatewayConfig();
       return adminHelper.start({ config });
     });
+
     it('should create a new pipeline', () => {
       const testPipeline = {
         apiEndpoints: ['api'],
-        policies: [{ action: 'proxy' }],
+        policies: [{ proxy: { action: { serviceEndpoint: 'backend' } } }],
         customId: idGen.v4() // NOTE: save operation should allow custom props
       };
       return adminHelper.admin.config.pipelines
@@ -38,9 +43,9 @@ describe('REST: pipelines', () => {
         .then(() => {
           const data = fs.readFileSync(config.gatewayConfigPath, 'utf8');
           const cfg = yaml.load(data);
-          assert.deepEqual(cfg.pipelines.test.apiEndpoints, testPipeline.apiEndpoints);
-          assert.deepEqual(cfg.pipelines.test.policies, testPipeline.policies);
-          assert(cfg.pipelines.test.customId);
+          should(cfg.pipelines.test.apiEndpoints).deepEqual(testPipeline.apiEndpoints);
+          should(cfg.pipelines.test.policies).deepEqual(testPipeline.policies);
+          should(cfg.pipelines.test).have.property('customId');
         });
     });
   });
@@ -49,45 +54,81 @@ describe('REST: pipelines', () => {
     beforeEach(() => {
       const initialConfig = {
         admin: { port: 0 },
+        apiEndpoints: { api: { host: '*' } },
+        serviceEndpoints: { backend: { url: 'http://localhost:1010' } },
+        policies: ['proxy', 'terminate'],
         pipelines: {
-          example: { apiEndpoints: ['example'] },
-          hello: { apiEndpoints: ['hello'] }
+          example: { apiEndpoints: ['example'], policies: [{ terminate: {} }] },
+          hello: { apiEndpoints: ['hello'], policies: [{ terminate: {} }] }
         }
       };
       fs.writeFileSync(config.gatewayConfigPath, yaml.dump(initialConfig));
       config.loadGatewayConfig();
       return adminHelper.start({ config });
     });
+
     it('should create a new pipeline', () => {
       const testPipeline = {
         apiEndpoints: ['api'],
         customId: idGen.v4(), // NOTE: save operation should allow custom props
-        policies: [{ action: 'proxy' }]
+        policies: [{ proxy: { action: { serviceEndpoint: 'backend' } } }]
       };
+
       return adminHelper.admin.config.pipelines
         .create('test', testPipeline)
         .then(() => {
           const data = fs.readFileSync(config.gatewayConfigPath, 'utf8');
           const cfg = yaml.load(data);
-          assert.deepEqual(cfg.pipelines.test.apiEndpoints, testPipeline.apiEndpoints);
-          assert.deepEqual(cfg.pipelines.example.apiEndpoints, ['example']);
-          assert.deepEqual(cfg.pipelines.hello.apiEndpoints, ['hello']);
-          assert(cfg.pipelines.test.customId);
+          should(cfg.pipelines.test.apiEndpoints).deepEqual(testPipeline.apiEndpoints);
+          should(cfg.pipelines.example.apiEndpoints).deepEqual(['example']);
+          should(cfg.pipelines.hello.apiEndpoints).deepEqual(['hello']);
+          should(cfg.pipelines.test).have.property('customId');
         });
     });
+
+    it('should not create a new pipeline when the general gateway.config is invalid', () => {
+      const testPipeline = {
+        apiEndpoints: ['api'],
+        customId: idGen.v4(), // NOTE: save operation should allow custom props
+        policies: ['proxy', 'terminate']
+      };
+      return adminHelper.admin.config.pipelines
+        .create('invalid', testPipeline)
+        .catch(() => {
+          const data = fs.readFileSync(config.gatewayConfigPath, 'utf8');
+          const cfg = yaml.load(data);
+          should(cfg.pipelines).not.have.property('invalid');
+        });
+    });
+
+    it('should not create a new pipeline when the a specified policy is invalid', () => {
+      const testPipeline = {
+        apiEndpoints: ['api'],
+        customId: idGen.v4(), // NOTE: save operation should allow custom props
+        policies: [{ proxy: {} }]
+      };
+      return adminHelper.admin.config.pipelines
+        .create('invalid', testPipeline)
+        .catch(() => {
+          const data = fs.readFileSync(config.gatewayConfigPath, 'utf8');
+          const cfg = yaml.load(data);
+          should(cfg.pipelines).not.have.property('invalid');
+        });
+    });
+
     it('should update existing pipeline', () => {
       const testPipeline = {
         apiEndpoints: ['api'],
         customId: idGen.v4(), // NOTE: save operation should allow custom props
-        policies: [{ action: 'proxy' }]
+        policies: [{ proxy: { action: { serviceEndpoint: 'backend' } } }]
       };
       return adminHelper.admin.config.pipelines
         .update('example', testPipeline)
         .then(() => {
           const data = fs.readFileSync(config.gatewayConfigPath, 'utf8');
           const cfg = yaml.load(data);
-          assert.deepEqual(cfg.pipelines.example.apiEndpoints, testPipeline.apiEndpoints);
-          assert(cfg.pipelines.example.customId);
+          should(cfg.pipelines.example.apiEndpoints).deepEqual(testPipeline.apiEndpoints);
+          should(cfg.pipelines.example).have.property('customId');
         });
     });
 
@@ -97,23 +138,25 @@ describe('REST: pipelines', () => {
         .then(() => {
           const data = fs.readFileSync(config.gatewayConfigPath, 'utf8');
           const cfg = yaml.load(data);
-          assert(!cfg.pipelines.example);
+          should(cfg.pipelines.example).not.ok();
         });
     });
+
     it('should show existing pipeline', () => {
       return adminHelper.admin.config.pipelines
         .info('example')
         .then((endpoint) => {
-          assert.deepEqual(endpoint.apiEndpoints, ['example']);
+          should(endpoint.apiEndpoints).be.deepEqual(['example']);
         });
     });
+
     it('should list all pipelines', () => {
       return adminHelper.admin.config.pipelines
         .list()
         .then((pipelines) => {
-          assert.deepEqual(pipelines.example.apiEndpoints, ['example']);
-          assert.deepEqual(pipelines.hello.apiEndpoints, ['hello']);
-          assert.equal(Object.keys(pipelines).length, 2);
+          should(pipelines.example.apiEndpoints).be.deepEqual(['example']);
+          should(pipelines.hello.apiEndpoints).be.deepEqual(['hello']);
+          should(Object.keys(pipelines)).length(2);
         });
     });
   });
