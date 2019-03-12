6f2a2217efede69ddec3abd0605e8d0918d85d2d
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-03-12 21:36:12 +0100

    minor typescript updates

--- index.d.ts
+++ index.d.ts
@@ -1,42 +1,39 @@
-import express = require("express");
+import * as express from "express";
 import { EventEmitter } from "events";
-import { JSONSchema6 } from "json-schema";
+import { JSONSchema7 } from "json-schema";
 
-declare global {
-  namespace Express {
-    export interface Request {
-      egContext: any
-    }
-  }
-
-  namespace ExpressGateway {
-    interface Policy {
-      name: string,
-      policy(actionParams): express.RequestHandler,
-      schema?: JSONSchema6
-    }
-
-    interface Condition {
-      name: string,
-      handler(req: express.Request, conditionConfig): boolean,
-      schema?: JSONSchema6
-    }
-
-    interface PluginContext {
-      registerPolicy(policy: Policy): void,
-      registerCondition(condition: Condition): void,
-      registerGatewayRoute(gatewayRoutesDeclaration: (gatewayExpressApp: express.Application) => void): void,
-      registerAdminRoute(adminRoutesDeclaration: (adminExpressApp: express.Application) => void): void,
-      registerCLIExtension(cliExtension): void,
-      eventBus: EventEmitter
-    }
-
-    export interface Plugin {
-      version?: string,
-      policies?: Array<string>,
-      init(context: PluginContext): void,
-      schema?: JSONSchema6
-    }
+declare namespace Express {
+  export interface Request {
+    egContext: any
   }
 }
 
+declare namespace ExpressGateway {
+  type Policy = {
+    name: string,
+    policy(actionParams): express.RequestHandler,
+    schema?: JSONSchema7
+  }
+
+  type Condition = {
+    name: string,
+    handler(req: express.Request, conditionConfig: any): boolean,
+    schema?: JSONSchema7
+  }
+
+  type PluginContext = {
+    registerPolicy(policy: Policy): void,
+    registerCondition(condition: Condition): void,
+    registerGatewayRoute(gatewayRoutesDeclaration: (gatewayExpressApp: express.Application) => void): void,
+    registerAdminRoute(adminRoutesDeclaration: (adminExpressApp: express.Application) => void): void,
+    registerCLIExtension(cliExtension): void,
+    eventBus: EventEmitter
+  }
+
+  export type Plugin = {
+    version?: string,
+    policies?: Array<string>,
+    init(context: PluginContext): void,
+    schema?: JSONSchema7
+  }
+}
