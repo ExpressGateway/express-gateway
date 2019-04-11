26e4b54283322158b5f4993599dd3bf40d4cc339
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-04-11 13:03:09 +0200

    do not overwrite create the temp user

--- test/services/users.test.js
+++ test/services/users.test.js
@@ -221,8 +221,7 @@ describe('User service tests', () => {
   describe('Activate and deactivate user tests', () => {
     let user;
     before(() => db.flushdb().then(() => {
-      user = createRandomUserObject();
-      return userService.insert(user);
+      return userService.insert(createRandomUserObject());
     }).then(newUser => {
       user = newUser; // update test user
       should.exist(newUser);
@@ -326,7 +325,7 @@ describe('User service tests', () => {
   });
 });
 
-function createRandomUserObject () {
+function createRandomUserObject() {
   return {
     username: uuid.v4(),
     firstname: uuid.v4(),
