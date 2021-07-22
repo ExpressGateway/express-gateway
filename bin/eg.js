b7932cef3ed773bd845a62009ea53325b00883aa
Brian Houser <brian.houser@mappedin.ca>
2021-07-21 22:21:12 -0400

    remove cli and yeoman-generator features due to dependency security vulnerabilities

--- bin/eg.js
+++
@@ -1,1 +0,0 @@
-exports.Generator = require('./eg-generator');
--- bin/generators/gateway/templates/basic/server.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const gateway = require('express-gateway');
-
-gateway()
-  .load(path.join(__dirname, 'config'))
-  .run();
